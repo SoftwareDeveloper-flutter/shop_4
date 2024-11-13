fbe1a1900501f3c298d50fb6cae1be80df35bded
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-11-13 11:50:40 +0500

    updated

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 class DetailScreen extends StatefulWidget{
   const DetailScreen({super.key});
+  
  @override
   State<StatefulWidget>createState(){
       return DetailScreenState();
@@ -8,12 +9,82 @@ class DetailScreen extends StatefulWidget{
 }
 
 class DetailScreenState extends State<DetailScreen>{
+   int counterQty=1;
+
+
+  void _addQty(){
+   setState((){
+      counterQty++;
+   });
+  }
+
+  void _removeQty(){
+   setState((){
+    if(counterQty > 1){
+      counterQty--;
+    }
+   });
+  }
+
   @override
   Widget build(BuildContext context){
     return  Scaffold(
          appBar: AppBar(
-          title:const Text("Product Details")
+      
          ),
+         body: Column(
+             children: [
+              
+              Image.asset('assets/images/shoe.jpg'),
+              Padding(
+              padding:const EdgeInsets.only(left:10.0,top:10.0),
+              child:Row(
+                children: [
+                  const Text("Axel Arigoto",style:TextStyle(fontSize:17,fontWeight:FontWeight.w900)),
+                   const SizedBox(width:50),
+                  Container(
+          width:120,height:30,
+          decoration:BoxDecoration(
+              color:const Color.fromARGB(255, 201, 188, 188),
+              borderRadius: BorderRadius.circular(10.0),
+          ),
+        
+          child:  Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              IconButton( 
+                icon:const Icon(Icons.add),
+                onPressed: _addQty,
+                 padding: EdgeInsets.zero,
+                ),
+              
+               Text("$counterQty"),
+                IconButton(
+                  icon: const Icon(Icons.remove),
+                  onPressed: _removeQty,
+                   padding: EdgeInsets.zero,
+                  )
+
+            ],
+          ),
+  ),
+                ],
+              ),
+             )
+          
+              
+
+               
+              
+              
+             ],
+             
+             ),
+             
+      
+
     );
   }
 }
+
