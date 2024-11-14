8705eb67d1ca0c180e66461c080552e8d6d7b6ff
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-11-14 12:15:46 +0500

    updated

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -41,7 +41,7 @@ class DetailScreenState extends State<DetailScreen>{
               child:Row(
                 children: [
                   const Text("Axel Arigoto",style:TextStyle(fontSize:17,fontWeight:FontWeight.w900)),
-                   const SizedBox(width:50),
+                   const SizedBox(width:85),
                   Container(
           width:120,height:30,
           decoration:BoxDecoration(
@@ -71,12 +71,54 @@ class DetailScreenState extends State<DetailScreen>{
   ),
                 ],
               ),
-             )
+             ),
+
           
               
-
+        const Padding(
+          
+          padding: EdgeInsets.only(left:10.0),
+            child: Align(
+            alignment: Alignment.centerLeft,
+          child:Text("Clean 90 Triple Sneakers")
+            ),
+          ),
                
-              
+                const Padding(
+          
+          padding: EdgeInsets.only(left:10.0),
+            child: Align(
+            alignment: Alignment.centerLeft,
+            child:Row(
+              children: [
+               Icon(Icons.star,color:Colors.yellow),
+               Icon(Icons.star,color:Colors.yellow),
+               Icon(Icons.star,color:Colors.yellow),
+               Icon(Icons.star,color:Colors.yellow),
+               Icon(Icons.star,color:Colors.yellow),
+               SizedBox(width:10.0),
+               Text("(270 Review)",style:TextStyle(fontWeight:FontWeight.bold))
+              ],
+            ),
+
+         
+            ),
+          ),
+          const Padding(
+            padding:EdgeInsets.only(top:10.0,left:10.0),
+            child:Align(
+               alignment: Alignment.centerLeft,
+               child:Text("Size",style:TextStyle(fontSize:17,fontWeight:FontWeight.bold))
+            ),
+          ),
+
+          const Padding(
+            padding:EdgeInsets.only(top:10.0,left:10.0),
+            child:Align(
+               alignment: Alignment.centerLeft,
+               child:Text("Description",style:TextStyle(fontSize:17,fontWeight:FontWeight.bold))
+            ),
+          ),
               
              ],
              
